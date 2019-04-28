71e0513f8cc403c18714700bfd78701fbe018ead
yamamotsu <motsulab@gmail.com>
2019-04-28 12:35:03 +0900

    update credentials_sample

--- program/main/credentials_sample.h
+++ program/main/credentials_sample.h
@@ -3,5 +3,9 @@
 
 const char *ssid = "XXXX";
 const char *wifi_pass = "XXXXX";
+const char* mqtt_server = "your.mqtt-server.com";
+const char* mqtt_user = "your-mqtt-username";
+const char* mqtt_password = "your-mqtt-password";
+const int mqtt_port = 00000;
 
 #endif
